f36e7370808ce31448e333e51dee067d260a66d2
Ricardo Ledesma <rickydem24@gmail.com>
2018-09-26 14:29:20 -0600

    added different currency denomations, new tests, and updated setup method in test suite

--- lib/bank.rb
+++ lib/bank.rb
@@ -14,40 +14,70 @@ class Bank
 
   def deposit(person, deposit)
     return no_account_message(person) if not has_account?(person)
-    if has_enough?(person, deposit)
+    if has_enough_cash?(person, deposit)
       add_to_account(person, deposit)
       remove_from_cash(person, deposit)
     else
-      not_enough_funds_message(person, 'deposit')
+      not_enough_cash_for_deposit_message(person, deposit)
     end
   end
 
-  def withdrawal(person, amount)
+  def withdrawal(person, withdrawal)
     return no_account_message(person) if not has_account?(person)
-    if has_enough_galleons_in_account?(person, amount)
-      person.bank_accounts[name] -= amount
-      person.galleons += amount
+    if has_enough_in_account?(person, withdrawal)
+      remove_from_account(person, withdrawal)
+      add_to_cash(person, withdrawal)
     else
-      not_enough_funds_message(person, 'withdrawal')
+      not_enough_in_account_message(person, withdrawal, :withdrawal)
     end
   end
 
-  def transfer(person, other_bank, amount)
+  def transfer(person, other_bank, transfer)
     if has_accounts_with_both?(person, other_bank)
-      if has_enough_galleons_in_account?(person, amount)
-        person.bank_accounts[name] -= amount
-        person.bank_accounts[other_bank.name] += amount
+      if has_enough_in_account?(person, transfer)
+        remove_from_account(person, transfer)
+        other_bank.add_to_account(person, transfer)
       else
-        not_enough_funds_message(person, 'transfer')
+        not_enough_in_account_message(person, transfer, :transfer)
       end
     else
       no_account_message_banks(person, other_bank)
     end
   end
 
+  def add_to_account(person, deposit)
+    deposit.each do |denomination, amount|
+      person.bank_accounts[name][denomination] += amount
+    end
+  end
+
+  def remove_from_account(person, withdrawal)
+    withdrawal.each do |denomination, amount|
+      person.bank_accounts[name][denomination] -= amount
+    end
+  end
+
+  def add_to_cash(person, withdrawal)
+    withdrawal.each do |denomination, amount|
+      person.cash[denomination] += amount
+    end
+  end
+
+  def remove_from_cash(person, deposit)
+    deposit.each do |denomination, amount|
+      person.cash[denomination] -= amount
+    end
+  end
+
   def total_cash(person)
     return no_account_message(person) if not has_account?(person)
-    "#{person.bank_accounts[name]} galleons"
+    total_cash = person.bank_accounts[name].reduce("") do |cash_string, (denomination, amount)|
+      cash_string += " and #{amount} #{denomination.to_s.sub('_', ' ')}" unless denomination == :galleons
+      if denomination == :galleons
+        cash_string += "#{person.name} has #{amount} #{denomination.to_s.sub('_', ' ')}"
+      end
+      cash_string
+    end
   end
 
   def has_account?(person)
@@ -70,8 +100,19 @@ class Bank
     end
   end
 
-  def not_enough_funds_message(person, transaction_type)
-    "#{person.name} does not have enough funds for this #{transaction_type}"
+  def not_enough_cash_for_deposit_message(person, deposit)
+    currency = deposit.find do |denomination, amount|
+      amount > person.cash[denomination]
+    end
+    "#{person.name} does not have enough #{currency[0].to_s.sub('_', ' ')} for this deposit"
+  end
+
+
+  def not_enough_in_account_message(person, withdrawal_or_transfer, type)
+    currency = withdrawal_or_transfer.find do |denomination, amount|
+      amount > person.bank_accounts[name][denomination]
+    end
+    "#{person.name} does not have enough #{currency[0].to_s.sub('_', ' ')} in their account for this #{type.to_s}"
   end
 
   def calculate_value(currencies)
@@ -99,15 +140,21 @@ class Bank
     end
   end
 
-  def has_enough?(person, deposit)
+  def has_enough_cash?(person, deposit)
     enough = true
     deposit.each do |denomination, amount|
+      enough = false if amount > person.cash[denomination]
+    end
+    enough
+  end
+
+  def has_enough_in_account?(person, withdrawal)
+    enough = true
+    withdrawal.each do |denomination, amount|
       enough = false if amount > person.bank_accounts[name][denomination]
     end
     enough
   end
 
-  def has_enough_galleons_in_account?(person, amount)
-    person.bank_accounts[name] >= amount
-  end
+
 end
--- test/bank_test.rb
+++ test/bank_test.rb
@@ -7,9 +7,12 @@ require_relative '../lib/person'
 
 class BankTest < Minitest::Test
   def setup
+    @person = Person.new('Ron', galleons: 10, silver_sickles: 15, bronze_knuts: 25)
+    @person_2 = Person.new('Harry', galleons: 10, silver_sickles: 15, bronze_knuts: 25)
     @bank = Bank.new('Chase')
     @bank_2 = Bank.new('Wells Fargo')
-    @person = Person.new('Ron', galleons: 10, silver_sickles: 15, bronze_knuts: 25)
+    @bank_2.open_account(@person_2)
+    @bank_2.deposit(@person_2, galleons: 5, silver_sickles: 10, bronze_knuts: 20)
   end
 
   def test_has_name
@@ -24,92 +27,143 @@ class BankTest < Minitest::Test
 
   def test_takes_a_deposit
     @bank.open_account(@person)
-    @bank.deposit(@person, galleons: 5, silver_sickles: 16)
-    assert_equal 50, @person.cash
-    assert_equal ({galleons: 5, silver_sickles: 16}), @person.bank_accounts[@bank.name]
+    @bank.deposit(@person, galleons: 5, silver_sickles: 10, bronze_knuts: 20)
+
+    assert_equal ({galleons: 5, silver_sickles: 5, bronze_knuts: 5 }),
+    @person.cash
+    assert_equal ({galleons: 5, silver_sickles: 10, bronze_knuts: 20}),
+    @person.bank_accounts[@bank.name]
+  end
+
+  def test_can_take_deposit_with_only_some_denominations
+    @bank.open_account(@person)
+    @bank.deposit(@person, galleons: 5, silver_sickles: 5)
+
+    assert_equal ({galleons: 5, silver_sickles: 5, bronze_knuts: 0}),
+    @person.bank_accounts[@bank.name]
+    assert_equal ({galleons: 5, silver_sickles: 10, bronze_knuts: 25}),
+    @person.cash
   end
 
   def test_cant_take_deposit_if_insufficient_galleons
     @bank.open_account(@person)
-    assert_equal "Ron does not have enough funds for this deposit",
-    @bank.deposit(@person, 200)
+    assert_equal "Ron does not have enough galleons for this deposit",
+    @bank.deposit(@person, galleons: 11, silver_sickles: 15, bronze_knuts: 25)
+  end
+
+  def test_cant_take_deposit_if_insufficient_silver_sickles
+    @bank.open_account(@person)
+    assert_equal "Ron does not have enough silver sickles for this deposit",
+    @bank.deposit(@person, galleons: 10, silver_sickles: 16, bronze_knuts: 25)
+  end
+
+  def test_cant_take_deposit_if_insufficient_bronze_knuts
+    @bank.open_account(@person)
+    assert_equal "Ron does not have enough bronze knuts for this deposit",
+    @bank.deposit(@person, galleons: 10, silver_sickles: 15, bronze_knuts: 26)
   end
 
   def test_cant_take_deposit_if_no_account
     assert_equal "Ron does not have an account with Chase",
-    @bank.deposit(@person, 50)
+    @bank.deposit(@person, galleons: 10, silver_sickles: 15, bronze_knuts: 25)
   end
 
   def test_can_make_a_withdrawal
-    @bank.open_account(@person)
-    @bank.deposit(@person, 50)
+    assert_equal ({galleons: 5, silver_sickles: 10, bronze_knuts: 20}),
+    @person_2.bank_accounts[@bank_2.name]
+    assert_equal ({galleons: 5, silver_sickles: 5, bronze_knuts: 5}),
+    @person_2.cash
 
-    assert_equal 50, @person.bank_accounts[@bank.name]
-    assert_equal 50, @person.galleons
+    @bank_2.withdrawal(@person_2, galleons: 2, silver_sickles: 2, bronze_knuts: 10)
+    assert_equal ({galleons: 3, silver_sickles: 8, bronze_knuts: 10}),
+    @person_2.bank_accounts[@bank_2.name]
 
-    @bank.withdrawal(@person, 50)
-    assert_equal 0, @person.bank_accounts[@bank.name]
-    assert_equal 100, @person.galleons
+    assert_equal ({galleons: 7, silver_sickles: 7, bronze_knuts: 15}),
+    @person_2.cash
+  end
+
+
+  def test_can_make_a_withdrawal_with_only_some_denominations
+    assert_equal ({galleons: 5, silver_sickles: 10, bronze_knuts: 20}),
+    @person_2.bank_accounts[@bank_2.name]
+    assert_equal ({galleons: 5, silver_sickles: 5, bronze_knuts: 5}),
+    @person_2.cash
+
+    @bank_2.withdrawal(@person_2, galleons: 2, bronze_knuts: 10)
+    assert_equal ({galleons: 3, silver_sickles: 10, bronze_knuts: 10}),
+    @person_2.bank_accounts[@bank_2.name]
+
+    assert_equal ({galleons: 7, silver_sickles: 5, bronze_knuts: 15}),
+    @person_2.cash
   end
 
   def test_cant_overwithdraw_account
-    @bank.open_account(@person)
-    @bank.deposit(@person, 50)
+    assert_equal "Harry does not have enough galleons in their account for this withdrawal",
+    @bank_2.withdrawal(@person_2, galleons: 7, silver_sickles: 5, bronze_knuts: 10)
+  end
 
-    assert_equal 50, @person.bank_accounts[@bank.name]
-    assert_equal 50, @person.galleons
-
-    assert_equal "Ron does not have enough funds for this withdrawal",
-    @bank.withdrawal(@person, 100)
+  def test_cant_overwithdraw_account_different_denomination
+    assert_equal "Harry does not have enough silver sickles in their account for this withdrawal",
+    @bank_2.withdrawal(@person_2, galleons: 2, silver_sickles: 11, bronze_knuts: 10)
   end
 
   def test_cant_withdraw_if_no_account
     assert_equal "Ron does not have an account with Chase",
-    @bank.withdrawal(@person, 100)
+    @bank.withdrawal(@person, galleons: 5, silver_sickles: 5, bronze_knuts: 15)
   end
 
   def test_can_transfer_money_to_account
-    @bank.open_account(@person)
-    @bank_2.open_account(@person)
-    @bank.deposit(@person, 50)
+    @bank.open_account(@person_2)
+    @bank.deposit(@person_2, galleons: 5, silver_sickles: 5, bronze_knuts: 5)
 
-    assert_equal 50, @person.bank_accounts[@bank.name]
-    @bank.transfer(@person, @bank_2, 50)
-    assert_equal 0, @person.bank_accounts[@bank.name]
-    assert_equal 50, @person.bank_accounts[@bank_2.name]
+    assert_equal ({galleons: 5, silver_sickles: 5, bronze_knuts: 5}),
+    @person_2.bank_accounts[@bank.name]
+
+    @bank.transfer(@person_2, @bank_2, {galleons: 2, silver_sickles: 2, bronze_knuts: 2})
+    assert_equal ({galleons: 3, silver_sickles: 3, bronze_knuts: 3}),
+    @person_2.bank_accounts[@bank.name]
+    assert_equal ({galleons: 7, silver_sickles: 12, bronze_knuts: 22}),
+    @person_2.bank_accounts[@bank_2.name]
   end
 
   def test_cant_transfer_money_if_insufficient_funds
-    @bank.open_account(@person)
-    @bank_2.open_account(@person)
-    @bank.deposit(@person, 50)
+    @bank.open_account(@person_2)
+    @bank.deposit(@person_2, galleons: 5, silver_sickles: 5, bronze_knuts: 5)
 
-    assert_equal 50, @person.bank_accounts[@bank.name]
-    assert_equal "Ron does not have enough funds for this transfer",
-    @bank.transfer(@person, @bank_2, 100)
+    assert_equal ({galleons: 5, silver_sickles: 5, bronze_knuts: 5}),
+    @person_2.bank_accounts[@bank.name]
+
+    assert_equal "Harry does not have enough galleons in their account for this transfer",
+    @bank.transfer(@person_2, @bank_2, {galleons: 6, silver_sickles: 2, bronze_knuts: 2})
+  end
+
+  def test_cant_transfer_money_if_insufficient_funds_other_denomination
+    @bank.open_account(@person_2)
+    @bank.deposit(@person_2, galleons: 5, silver_sickles: 5, bronze_knuts: 5)
+
+    assert_equal ({galleons: 5, silver_sickles: 5, bronze_knuts: 5}),
+    @person_2.bank_accounts[@bank.name]
+
+    assert_equal "Harry does not have enough silver sickles in their account for this transfer",
+    @bank.transfer(@person_2, @bank_2, {galleons: 2, silver_sickles: 6, bronze_knuts: 2})
   end
 
   def test_cant_transfer_money_if_no_account_with_current_bank
-    @bank_2.open_account(@person)
-
-    assert_equal 0, @person.bank_accounts[@bank_2.name]
-    assert_equal "Ron does not have an account with Chase",
-    @bank.transfer(@person, @bank_2, 50)
+    assert_equal "Harry does not have an account with Chase",
+    @bank.transfer(@person_2, @bank_2, galleons: 2, silver_sickles: 2, bronze_knuts: 2)
   end
 
   def test_cant_transfer_money_if_no_account_with_other_bank
     @bank.open_account(@person)
 
-    assert_equal 0, @person.bank_accounts[@bank.name]
+    assert_equal ({galleons: 0, silver_sickles: 0, bronze_knuts: 0}),
+    @person.bank_accounts[@bank.name]
     assert_equal "Ron does not have an account with Wells Fargo",
-    @bank_2.transfer(@person, @bank, 50)
+    @bank_2.transfer(@person, @bank, galleons: 2, silver_sickles: 2, bronze_knuts: 2)
   end
 
   def test_can_retrieve_total_cash_from_account
-    @bank.open_account(@person)
-    @bank.deposit(@person, 50)
-
-    assert_equal 50, @person.bank_accounts[@bank.name]
-    assert_equal "50 galleons", @bank.total_cash(@person)
+    assert_equal "Harry has 5 galleons and 10 silver sickles and 20 bronze knuts",
+    @bank_2.total_cash(@person_2)
   end
 end
